c5abd5bfc41990112717a0a0fa87ad57325f9c32
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2019-05-28 15:53:05 +1000

    new environemtnal threshold generation/data

--- vegplot_internal_tools/prep_env_thresholds.R
+++ vegplot_internal_tools/prep_env_thresholds.R
@@ -1,7 +1,18 @@
-raw_thresholds <- read.csv("raw_data/envoutlierthresholds_5Mar2019.csv", stringsAsFactors = F)
+library(dplyr)
 
-raw_thresholds$gp <- gsub(".", "_", raw_thresholds$gp, fixed = T)
+env_range <- read.csv("raw_data/envoutlierthresholds_5Mar2019.csv", stringsAsFactors = F) %>%
+  select(-freq) %>%
+  rename(group = gp)
+env_range$group <- gsub(".", "_", env_range$group, fixed = T)
 
-raw_thresholds$freq <- NULL
 
-saveRDS(raw_thresholds, file = "tool_inputs/env_thresholds.rds")
+env_minmax <- read.csv("raw_data/RCPGrpEnvKnownRanges_FromPlotAssignMasterRound4_V37_PrimSecOnly_SentMitch23May2019.csv", stringsAsFactors = F) %>%
+  select(-Elevation_Med,-RainfallAnn_Med,-TempAnn_Med) %>%
+  rename(group = Row.Labels)
+env_minmax$group <- gsub(".", "_", env_minmax$group, fixed = T)
+
+
+env_thresh <- inner_join(env_range, env_minmax, by = "group")
+
+
+saveRDS(env_thresh, file = "tool_inputs/env_thresholds.rds")
